9a04e1221664f0b395fb6cd5df1e5f065336ca0e
James 'Eddy' Edwards <james.edwards@datasift.com>
2013-06-05 14:34:23 +0100

    Remove custom thumb reference until YARPP is updated

--- wp-content/themes/sidetracked/yarpp-template-thumbnail.php
+++ wp-content/themes/sidetracked/yarpp-template-thumbnail.php
@@ -9,7 +9,7 @@ Author: James 'Eddy' Edwards
 		<?php while (have_posts()) : the_post(); ?>
 			<div class="span4">
 				<a class="post-thumb" href="<?php the_permalink(); ?>">
-					<?php echo ah_get_custom_thumb(); ?>
+					<?php //echo ah_get_custom_thumb(); ?>
 					<span class="title"><?php the_title(); ?></span>
 				</a>
 				<span class="excerpt"><?php echo strip_tags(get_the_excerpt()) ?>...</span>
