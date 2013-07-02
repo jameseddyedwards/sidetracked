8f90fd56e3ef96b1f8b8b793020aed0cbc17b53d
James 'Eddy' Edwards <james.edwards@datasift.com>
2013-07-02 20:21:53 +0100

    Hide Posted On for Mobile/Tablet

--- wp-content/themes/sidetracked/category-news.php
+++ wp-content/themes/sidetracked/category-news.php
@@ -40,7 +40,7 @@ echo $wp_query->max_num_pages;
 
 				<!-- Date & Image -->
 				<div class="block cf">
-					<div class="span two posted-on">
+					<div class="span two posted-on visible-desktop">
 						<?php sidetracked_posted_on(); ?>
 					</div>
 					<div class="span eight news-article-image">
@@ -48,18 +48,18 @@ echo $wp_query->max_num_pages;
 							<img src="<?php echo $featureImageObj['sizes']['rectangle-medium']; ?>" alt="<?php the_title(); ?>" />
 						</a>
 					</div>
-					<div class="span two">&nbsp;</div>
+					<div class="span two visible-desktop">&nbsp;</div>
 				</div>
 
 				<!-- Title, Excerpt & Read More -->
 				<div class="block cf">
-					<div class="span two">&nbsp;</div>
+					<div class="span two visible-desktop">&nbsp;</div>
 					<div class="span eight">
 						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 						<p><?php the_excerpt(); ?></p>
 						<a class="read-more" href="<?php the_permalink(); ?>">read more &#187;</a>
 					</div>
-					<div class="span two">&nbsp;</div>
+					<div class="span two visible-desktop">&nbsp;</div>
 				</div>
 
 			</article>
@@ -67,22 +67,22 @@ echo $wp_query->max_num_pages;
 		<?php endwhile; ?>
 
 		<div class="block">
-			<div class="span two">&nbsp;</div>
+			<div class="span two visible-desktop">&nbsp;</div>
 			<div class="span eight">
 				<?php if (function_exists('wp_paginate')) { wp_paginate(); } ?>
 			</div>
-			<div class="span two">&nbsp;</div>
+			<div class="span two visible-desktop">&nbsp;</div>
 		</div>
 
 	<?php else : ?>
 
 		<div class="block">
-			<div class="span two">&nbsp;</div>
+			<div class="span two visible-desktop">&nbsp;</div>
 			<div class="span eight">
 				<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
 				<?php get_search_form(); ?>
 			</div>
-			<div class="span two">&nbsp;</div>
+			<div class="span two visible-desktop">&nbsp;</div>
 		</div>
 
 	<?php endif; ?>
